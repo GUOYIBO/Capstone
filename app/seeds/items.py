9a9f5b159c8e5e59f4e9eba2a44182121044887e
Yibo <yiboguo@yahoo.com>
2022-10-28 19:26:36 -0700

    remove item seeder data

--- app/seeds/items.py
+++ app/seeds/items.py
@@ -4,16 +4,4 @@ from .category import categories
 
 
 
-items = [
-    ['Whole Grains Bread','Bagel', 'Scones', 'Donuts','Burrito', 'Tiramisu Cake Slice'],
-    ['Coconut Milk', 'Broth Chicken', 'Black Beans', 'Garbanzo Beans', 'Dark Red Kidney Beans'],
-    ['Shrimp', 'Salmon Fillet', 'Sea Bass Fillet', 'Sea Scallops', 'Lobster', 'Crab'],
-    ['Beef', 'Chicken', 'Lamb', 'Beef Groud', 'Turkey', 'Bacon', 'Steak'],
-    ['Green Peas', 'Yellow Corn', 'Mixed Vegetables', 'Carrots and Peas', 'Wild Blueberries'],
-    ['1 Dozen Eggs', 'Egg White' ],
-    ['Whole milk', 'Half & Half', 'Viping Cream', 'Cream Cheese'],
-    ['Apple', 'Banana', 'Blueberry', 'Chery', 'Mango', 'Orange', 'Pear', 'Strawberry', 'Watermelon']
-    ['Vodka', 'Blue Moon 12pk, 12oz bottles','Chardonnay Whilte Wine']
-]
-
 
